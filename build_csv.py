d672beb40744ced50fc70ae58ffb9f2fe5ce8e7e
SnShine <shine123surya@gmail.com>
2015-04-11 22:13:06 +0530

    add comments to the supporting files!

--- build_csv.py
+++ build_csv.py
@@ -1,16 +1,24 @@
+# Surya Teja Cheedella
+# shine123surya[at]gmail[dot]com
+# BITS Pilani, Hyderabad Campus
+
 from os import listdir
 
 if __name__== "__main__":
+	# output csv file where we save paths to out images
 	write_file= open("data.csv", "w")
 	data_dir= "/home/suryateja/Documents/GitHub/FaceRecognizer/output_images"
 
+	# folders for each subject in root images folder
 	subjects= listdir(data_dir)
-	print(subjects)
+	#print(subjects)
 
 	for i in range(len(subjects)):
+		# all pictures in a particular folder/subject
 		photos= listdir(data_dir+ "/"+ subjects[i])
 		for photo in photos:
 			out_line= data_dir+ "/"+ subjects[i]+ "/"+ photo+ ";"+ str(i)
+			# write the path and class of each picture to the csv file
 			write_file.write(out_line)
 			write_file.write("\n")
 			print(out_line)
--- detect_save_images.py
+++ detect_save_images.py
@@ -1,18 +1,26 @@
+# Surya Teja Cheedella
+# shine123surya[at]gmail[dot]com
+# BITS Pilani, Hyderabad Campus
+
 import cv2
 from os import listdir
 
 def cropImage(img, box):
 	[p, q, r, s]= box
+	# crop and save the image provided with the co-ordinates of bounding box
 	write_img_color= img[q:q+ s, p:p+ s]
 	saveCropped(write_img_color, i)
 
+# save the cropped image at specified location
 def saveCropped(img, i):
 	cv2.imwrite(output_path+ str(i)+ ".jpg", img)
 
 if __name__== "__main__":
+	# load pre-trained frontalface cascade classifier
 	frontal_face= cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 	input_names= listdir("/home/suryateja/Documents/GitHub/FaceRecognizer/input_images")
 
+	# paths to input and output images
 	input_path= "input_images/"
 	output_path= "output_images/"
 
@@ -20,12 +28,15 @@ if __name__== "__main__":
 	for name in input_names:
 		print(input_path+name)
 		color_img= cv2.imread(input_path+ name)
+		# converting color image to grayscale image
 		gray_img= cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
 
+		# find the bounding boxes around detected faces in images
 		bBoxes= frontal_face.detectMultiScale(gray_img, 1.3, 5)
 		print(bBoxes)
 
 		for box in bBoxes:
 			#print(box)
+			# crop and save the image at specified location
 			cropImage(color_img, box)
 			i+= 1
